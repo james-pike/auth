b50ceb0569532df292ba8ea02f815c714ecd0670
your name <you@exammple.com>
2025-09-10 17:29:41 -0400

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
+// vite.config.ts (root)
 import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
-import { vercel } from "@builder.io/qwik-city/adapters/vercel"; // Updated import
 import tsconfigPaths from "vite-tsconfig-paths";
 import pkg from "./package.json";
 
@@ -16,7 +15,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
 
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), vercel({ edge: true }), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     optimizeDeps: {
       exclude: [],
     },
@@ -30,6 +29,9 @@ export default defineConfig(({ command, mode }): UserConfig => {
         "Cache-Control": "public, max-age=600",
       },
     },
+    ssr: {
+      noExternal: Object.keys(devDependencies || {}), // Bundle dev deps for Edge
+    },
   };
 });
 
@@ -37,18 +39,13 @@ export default defineConfig(({ command, mode }): UserConfig => {
 function errorOnDuplicatesPkgDeps(devDependencies: PkgDep, dependencies: PkgDep) {
   const duplicateDeps = Object.keys(devDependencies).filter((dep) => dependencies[dep]);
   const qwikPkg = Object.keys(dependencies).filter((value) => /qwik/i.test(value));
-
   let msg = "";
   if (qwikPkg.length > 0) {
     msg = `Move Qwik packages ${qwikPkg.join(", ")} to devDependencies`;
     throw new Error(msg);
   }
-
   if (duplicateDeps.length > 0) {
-    msg = `
-      Warning: The dependency "${duplicateDeps.join(", ")}" is listed in both "devDependencies" and "dependencies".
-      Please move the duplicated dependencies to "devDependencies" only and remove it from "dependencies"
-    `;
+    msg = `Warning: The dependency "${duplicateDeps.join(", ")}" is listed in both "devDependencies" and "dependencies". Please move to "devDependencies" only.`;
     throw new Error(msg);
   }
 }
